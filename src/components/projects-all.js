ea4137cd4bcdac6c87141eed5ce7e086b155110a
Ida Adjivon <iadjivon@bu.edu>
2021-01-21 01:25:33 -0500

    changes

--- src/components/projects-all.js
+++ src/components/projects-all.js
@@ -4,4 +4,12 @@ import React from 'react'
     <h1>
         This is the contents of the Projects page.
     </h1>
+
+    <h1>
+        Our Stories
+    </h1>
+
+    <h1>
+        The VAW Journal
+    </h1>
 </div>
